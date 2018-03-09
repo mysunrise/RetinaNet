cec2c0c00c3bfe5692ff7520aef12934f080defc
mysunrise <10110907@sjtu.edu.cn>
2018-03-09 21:12:56 +0800

    change focalloss

--- focalloss.py
+++ focalloss.py
@@ -1,9 +1,9 @@
 '''
 
 args:
-input: N * num_classes
-target: N * 1
-alpha(data balance parameter): num_classes * 1
+input: (N, num_classes)
+target: (N, 1)
+alpha(data balance parameter): (num_classes, 1)
 gamma(weight decay parameter): scalar
 
 output:
@@ -11,9 +11,6 @@ focalloss
 
 '''
 
-import warnings
-
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
@@ -21,11 +18,15 @@ from torch.autograd import Variable
 
 class FocalLoss(nn.Module):
 
-    def __init__(self, alpha=None, gamma=2, size_average=True):
+    def __init__(self, alpha=None, gamma=2, size_average=False):
         super(FocalLoss, self).__init__()
-        self.alpha = alpha
-        if torch.is_tensor(alpha):
-            self.alpha = Variable(alpha)
+        if alpha is not None:
+            if isinstance(alpha, Variable):
+                self.alpha = alpha
+            else:
+                self.alpha = Variable(alpha)
+        else:
+            self.alpha = None
         self.gamma = gamma
         self.size_average = size_average
 
@@ -35,17 +36,20 @@ class FocalLoss(nn.Module):
             raise ValueError('Expected 2 dimensions (got {})'.format(dim))
         target = target.view(-1, 1)
 
-        log_p = F.log_softmax(input)
+        log_p = F.log_softmax(input, dim=1)
         log_p = log_p.gather(1, target)
         log_p = log_p.view(-1, 1)
         p = Variable(log_p.data.exp())
 
         if self.alpha is not None:
-            al = self.alpha.gather(0, target.data.view(-1))
-            log_p = log_p * Variable(al)
+            if input.is_cuda and not self.alpha.is_cuda:
+                self.alpha = self.alpha.cuda()
+            al = self.alpha.gather(0, target)
+            al = al.view(-1, 1)
+            log_p = log_p * al
         
-        loss = -1 * (1 - p) ** self.gamma * log_p
-        
+        loss = -1 * pow(1 - p, self.gamma) * log_p
+
         if self.size_average:
             return loss.mean()
         else:
